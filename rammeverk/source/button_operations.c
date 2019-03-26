9658e837a525bfd76a63eb2e9441bce34911692a
Yathuwaran <yathuwaran97@gmail.com>
2019-03-26 11:38:06 +0100

    yo

--- rammeverk/source/button_operations.c
+++ rammeverk/source/button_operations.c
@@ -4,7 +4,7 @@
 
 void read_set_button_lights(){
   for (int floor = 0, floor< N_FLOORS, floor++ ){
-    for (elev_button_type_t button, button < 3, button++){
+    for (elev_button_type_t button = BUTTON_CALL_UP, button < 3, button++){
       if (elev_get_button_signal(button,floor)){
         elev_set_button_lamp(button, floor, 1);
       }
@@ -12,6 +12,16 @@ void read_set_button_lights(){
   }
 }
 
-void reset_all_lights_but_stop();
+void reset_all_lights_but_stop(){
+  for (int floor = 0, floor< N_FLOORS, floor++ ){
+    for (elev_button_type_t button = BUTTON_CALL_UP, button < 3, button++){
+      elev_set_button_lamp(button,floor,0))
+    }
+  }
+}
 
-void reset_this_floor_light();
+void reset_this_floor_light( unsigned int floor){
+  for (elev_button_type_t button = BUTTON_CALL_UP, button < 3, button++){
+    elev_set_button_lamp(button,floor,0))
+  }
+}
