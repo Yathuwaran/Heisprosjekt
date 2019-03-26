29fee0b30a1656a6d07426c9748e9e423183a99f
Yathuwaran <yathuwaran97@gmail.com>
2019-03-26 11:37:43 +0100

    halla

--- rammeverk/source/controller.c
+++ rammeverk/source/controller.c
@@ -10,3 +10,10 @@ void stop_elevator(){
 ----------------------------------------------*/
 
 }
+
+struct controller {
+  int queue[4][3] = {};
+  elev_motor_direction_t dir;
+  unsigned int current_floor;
+  
+}
