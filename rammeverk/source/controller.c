0026816f374abd9010750559c8dd3be263b589f9
JonasThorset <jonathor@stud.ntnu.no>
2019-03-26 10:37:51 +0100

    hei

--- rammeverk/source/controller.c
+++ rammeverk/source/controller.c
@@ -1,1 +1,12 @@
 #include controller.h
+#include door.h
+#include button_operations.h
+
+void stop_elevator(){
+        reset_all_lights_but_stop();
+        elev_set_motor_direction(DIRN_STOP);
+/*---------------------------------------------
+        Delete queue here:
+----------------------------------------------*/
+
+}
