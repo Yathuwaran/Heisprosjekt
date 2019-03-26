342289faf60ba2d69334f2743098e0d9a0411d3b
Yathuwaran <yathuwaran97@gmail.com>
2019-03-26 10:30:40 +0100

    Temp

---
+++ rammeverk/source/button_operations.h
@@ -0,0 +1,10 @@
+
+
+//Reads buttons and lights up necessary buttons
+void read_set_button_lights();
+
+//Resets all of the lights, except the stop-light
+void reset_all_lights_but_stop();
+
+//Resets the light when the floor is reached
+void reset_this_floor_light();
