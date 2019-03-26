9778253a28b4a50215d3c819f338a497d3455dd1
Yathuwaran <yathuwaran97@gmail.com>
2019-03-26 10:29:59 +0100

    Door

--- rammeverk/source/door.c
+++ rammeverk/source/door.c
@@ -1,8 +1,13 @@
 #include door.h
 #include <unistd.h>
 
-void open_door() {
+void open_close_door() {
   elev_set_door_open_lamp(1);
   sleep(3);
+  while (elev_get_obstruction_signal(1)){
+    sleep(3);
+  }
+  
   elev_set_door_open_lamp(0);
+
 }
