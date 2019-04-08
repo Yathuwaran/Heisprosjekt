5a86e2afd60f7b54699908f8653dc7e332e35410
Yathuwaran <yathuwaran97@gmail.com>
2019-04-08 18:44:33 +0200

    doxygen queue done

--- rammeverk/source/queue.h
+++ rammeverk/source/queue.h
@@ -1,24 +1,54 @@
+/**
+* @file
+* @brief The queue file includes funtions used to manipulate the queue for the elevator, determine direction
+* based on elements in queue and check if the queue is empty.
+*/
+
 #ifndef __INCLUDE_queue_H__
 #define __INCLUDE_queue_H__
 #include "elev.h"
 #include "controller.h"
 #include <stdbool.h>
 
+/**
+* @brief Determines the next direction for the elevator, given that the previous state was stop.
+* The variables elevator-prev_dir and elevator->prev_floor from status* elevator are used to determine the direction
+* based on the orders in the queue.
+*
+* @param[out] status* elevator Struct for elevator status.
+*/
 int stop_dir(status*elevator);
 
-/*Checks and returns next direction for elevator based on queue matrix from
-controller.h. The function does not consider if the elevator shold stop or not
+/**
+* @brief Determines the next direction for the elevator. The previuos state is not taken into considerantion.
+* The variable elevator->current_floor and elevator->prev_dir are used together with the stop_dir(status* elevator)
+* funtion to determine the direction
+*
+* @param[out] status* elevator Struct for elevator status.
 */
 elev_motor_direction_t determine_dir(status* elevator);
 
-//Adds floor call to the queue
+/**
+* @brief Adds orders to the 4x3 2D-array called queue in status* elevator.
+* The orders are added by setting the value "1" in the array if an order button is pressed.
+*
+* @param[out] status* elevator Struct for elevator status.
+*/
 void add_to_queue(status* elevator);
 
-/*Checks if the queue is empty and returns true "1", and false if the queue
-is not empty*/
+/**
+* @brief Goes through the 4x3 2D-array and checks if every value is 0.
+*
+* @param[out] status* elevator Struct for elevator status.
+* @return TRUE if the array is empty, FALSE if not empty
+*/
 bool is_queue_empty(status* elevator);
 
-//Resets all button calls in queue
+/**
+* @brief Goes through the 4x3 2D-array and removes every order on the current floor.
+*
+* @param[out] status* elevator Struct for elevator status.
+*/
 void remove_current_floor_from_queue(status* elevator);
 
 #endif
