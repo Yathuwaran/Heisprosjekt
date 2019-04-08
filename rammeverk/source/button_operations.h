9c09f24d24b3df426bdf0e894a5a8a650f3d7417
Yathuwaran <yathuwaran97@gmail.com>
2019-04-08 19:08:03 +0200

    doxygen button_operations done

--- rammeverk/source/button_operations.h
+++ rammeverk/source/button_operations.h
@@ -1,13 +1,32 @@
+/**
+* @file
+* @brief Includes funtions for setting and resetting the button lamps on the elevator panel.
+*/
 #ifndef __INCLUDE_button_operations_H__
 #define __INCLUDE_button_operations_H__
 
 //Reads buttons and lights up necessary buttons
+
+/**
+* @brief Reads the button the button signals by going through the queue and sets the light
+* if the value in the array is 1.
+*/
 void read_set_button_lights();
 
 //Resets all of the lights, except the stop-light
+/**
+* @brief Goes through the queue and resets all of the button lights
+* Only the stop button light is still set.
+*/
 void reset_all_lights_but_stop();
 
-//Resets the light when the floor is reached
+//Resets all of the lights, except the stop-light
+/**
+* @brief Goes through the queue and resets all of the button lights
+* on the floor which is taken in as an argument in the function.
+*
+*@param[in] flr - The value of the relevant floor. 
+*/
 void reset_current_floor_light(int flr);
 
 #endif
